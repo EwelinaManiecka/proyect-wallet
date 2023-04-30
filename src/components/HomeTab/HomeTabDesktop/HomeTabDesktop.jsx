e65abf1f045641eb481d92613ca9c819d64ca4d0
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-04-30 19:52:12 +0200

    Create HomeTabDesktop.jsx

---
+++ src/components/HomeTab/HomeTabDesktop/HomeTabDesktop.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import selectors from '../../../redux/auth/selectors';
+
+import './HomeTabDesktop.module.scss';
+
+const tableHeadData = ['Date', 'Type', 'Category', 'Comment', 'Sum', 'Balance'];
+
+export default function HomeTabDesktop() {
+    const arr = useSelector(selectors.getTransactionsUser);
+
+    return (
+        <>
+            <table className={'homeTab'}>
+                <thead className={'homeTab-head'}>
+                    <tr className={'homeTab-head_row'}>
+                        {tableHeadData.map(head => (
+                            <th className={'homeTab-head_data'} key={head}>
+                                {head}
+                            </th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody className={'homeTab-body'}>
+                    {arr?.map(
+                        ({ _id, type, date, money, category, comment, balance }) => (
+                            <tr key={_id} className={'homeTab-body_row'}>
+                                <td className={'homeTab-body_data'}>{date}</td>
+                                <td className={'homeTab-body_data'}>{type}</td>
+                                <td className={'homeTab-body_data'}>{category.name}</td>
+                                <td className={'homeTab-body_data'}>{comment}</td>
+                                <td
+                                    className={
+                                        type === '+'
+                                            ? 'homeTab-body_data homeTab-body_data--plus'
+                                            : 'homeTab-body_data homeTab-body_data--minus'
+                                    }
+                                >
+                                    {money}
+                                </td>
+                                <td className={'homeTab-body_data'}>{balance}</td>
+                            </tr>
+                        ),
+                    )}
+                </tbody>
+            </table>
+        </>
+    );
+}
