695bacca421c0656ed1aae2db3e076b6d82b4c86
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-05-02 11:48:50 +0200

    Create HomeTab.jsx

---
+++ src/components/HomeTab/HomeTab.jsx
@@ -0,0 +1,16 @@
+import Media from 'react-media';
+import HomeTabDesktop from './HomeTabDesktop';
+import HomeTabMobile from './HomeTabMobile';
+
+
+export default function HomeTab() {
+  return (
+    <Media
+      queries={{
+        mobile: '(max-width: 767px)',
+      }}
+    >
+      {({ mobile }) => <>{mobile ? <HomeTabMobile /> : <HomeTabDesktop />}</>}
+    </Media>
+  );
+}
