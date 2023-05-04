07d5cb55ff83145d1e880ee1011cb9a5528704a5
Jakub Turek <kubo.bubo@interia.pl>
2023-05-04 11:12:09 +0200

    modalAddTransaction
     design done

--- src/components/ModalAddTransaction/ModalAddTransaction.jsx
+++ src/components/ModalAddTransaction/ModalAddTransaction.jsx
@@ -1,20 +1,41 @@
 import css from './ModalAddTransaction.module.scss';
 import Button from 'common/Button/Button';
 import closeBtn from './../../images/close_btn.svg';
+import { useState } from 'react';
 
 export const ModalAddTransaction = () => {
+  const [checked, setChecked] = useState(false);
+
   return (
     <div className={css.overlay}>
-      <div className={css.modal}>
+      <form className={css.modal}>
         <h1 className={css.modal__title}>Add transaction</h1>
         <div className={css.switcherContainer}>
-          <div>Income</div>
+          <p className={!checked && css.green}>Income</p>
           <label className={css.switch}>
-            <input type="checkbox"></input>
+            <input
+              type="checkbox"
+              checked={checked}
+              onClick={() => setChecked(!checked)}
+              defaultChecked
+            ></input>
             <span className={css.slider}></span>
           </label>
-          <p>Expense</p>
+          <p className={checked && css.green}>Expense</p>
         </div>
+        <div className={css.inputcontainer}>
+          <input
+            type="number"
+            className={css.number}
+            placeholder="0.00"
+          ></input>
+          <input type="date" className={css.date}></input>
+        </div>
+        <input
+          type="text"
+          className={css.comment}
+          placeholder="Comment"
+        ></input>
         <div className={css.buttonsWrapper}>
           <Button
             classNameBtn={css.buttonsWrapper__addbutton}
@@ -34,7 +55,7 @@ export const ModalAddTransaction = () => {
           height="16"
           alt="closeBTN"
         ></img>
-      </div>
+      </form>
     </div>
   );
 };
--- src/redux/global/global-action.js
+++ src/redux/global/global-action.js
@@ -2,6 +2,7 @@ import { createAction, createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   isModalLogoutOpen: false,
+  idModalAddTransactionOpen: false,
 };
 
 export const resetState = createAction('global/resetState');
@@ -16,8 +17,12 @@ const globalSlice = createSlice({
     closeModalLogout(state) {
       state.isModalLogoutOpen = false;
     },
+    toggleModalAddTransaction(state) {
+      state.idModalAddTransactionOpen = !state.idModalAddTransactionOpen;
+    },
   },
 });
 
-export const { openModalLogout, closeModalLogout } = globalSlice.actions;
+export const { openModalLogout, closeModalLogout, toggleModalAddTransaction } =
+  globalSlice.actions;
 export const globalReducer = globalSlice.reducer;
