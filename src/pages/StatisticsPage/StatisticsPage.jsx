8b9f406a7d727493893c5fdc2dbb6896f8155501
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-05-05 20:30:49 +0200

    Update StatisticsPage.jsx

--- src/pages/StatisticsPage/StatisticsPage.jsx
+++ src/pages/StatisticsPage/StatisticsPage.jsx
@@ -1,5 +1,30 @@
 import Header from 'components/Header/Header';
-
+import { Navigation } from 'components/Navigation/Navigation';
+import { Balance }  from 'components/Balance/Balance';
+import { Currency } from 'components/Currency/Currency';
+import { ChartDoughnut } from '../../components/Chart/Chart';
+import css from './StatisticsPage.module.scss';
 export const StatisticsPage = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+    <div className={css.dashboard}>
+        <div>
+            <Navigation />
+            <Balance />
+            <Currency />
+        </div>
+      
+         
+         
+        <div className={css.dashboard__chart}>
+          <h2 className={css.chart__title}>
+            Statistics
+          </h2>
+          <ChartDoughnut />
+        </div>
+    </div>
+    </>
+    
+  )
 };
