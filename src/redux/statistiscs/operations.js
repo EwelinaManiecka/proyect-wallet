7d9b0fc8b57199ab9ec36148a64a25f56d0db2a5
Maksymilian Bilski <biger9@o2.pl>
2023-05-08 20:20:18 +0200

    Update operations.js

---
+++ src/redux/statistiscs/operations.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+const currencyInstance = axios.create({
+  baseURL: 'https://wallet.goit.ua',
+  timeout: '8000',
+  mode: 'cors',
+});
+
+
+export const transactionSummary = createAsyncThunk(
+  'transactions-summary',
+  async  ({ year, month }, thunkAPI) => {
+    try {
+      const response = await currencyInstance.get(`/api/transactions-summary?month=${month}&year=${year}`);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
