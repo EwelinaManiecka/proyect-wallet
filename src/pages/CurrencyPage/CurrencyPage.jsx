3409ec5f61eb269c4bc5dde9f6db0377b1abd4a1
Maksymilian Bilski <biger9@o2.pl>
2023-05-10 13:16:56 +0200

    Update CurrencyPage.jsx

--- src/pages/CurrencyPage/CurrencyPage.jsx
+++ src/pages/CurrencyPage/CurrencyPage.jsx
@@ -2,24 +2,44 @@ import { Header } from 'components/Header/Header';
 import { Navigation } from 'components/Navigation/Navigation';
 import { Currency } from 'components/Currency/Currency';
 import { Footer } from 'components/Footer/Footer';
+import Media from 'react-media';
+import { Fragment } from 'react';
 
 import css from './CurrencyPage.module.scss';
+import { DashboardPage } from 'pages/DashboardPage/DashboardPage';
+import { NavLink } from 'react-router-dom';
 
 export const CurrencyPage = () => {
   return (
-    <>
-      <Header />
-       <div className={css.currency}>
-          <div className={css.currency_section}>
-            <div>
-              <Navigation />
-            </div>
-            <div className={css.currency_table}>
-              <Currency />
-            </div>
-          </div>
-        </div>
-      <Footer />
-    </>
+    <Media
+      queries={{ mobile: '(max-width: 767px)', else: '(min-width: 768px)' }}
+    >
+      {matches => (
+        <Fragment>
+          {matches.mobile && (
+            <>
+              <Header />
+              <div className={css.currency}>
+                <div className={css.currency_section}>
+                  <div>
+                    <Navigation />
+                  </div>
+                  <div className={css.currency_table}>
+                    <Currency />
+                  </div>
+                </div>
+              </div>
+              <Footer />
+            </>
+          )}
+          ,
+          {matches.else && (
+            <>
+              <DashboardPage />
+            </>
+          )}
+        </Fragment>
+      )}
+    </Media>
   );
 };
