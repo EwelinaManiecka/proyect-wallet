0f21e3c60c015fc2f74a22d3363428640c4ab1ae
EwelinaManiecka <ewelina.maniecka@op.pl>
2023-05-10 16:35:05 +0200

    statisticPageChanges
    changes scss StatisticPage, currencyPage and DashboardPage

--- src/pages/CurrencyPage/CurrencyPage.jsx
+++ src/pages/CurrencyPage/CurrencyPage.jsx
@@ -3,22 +3,12 @@ import { Navigation } from 'components/Navigation/Navigation';
 import { Currency } from 'components/Currency/Currency';
 import { Footer } from 'components/Footer/Footer';
 
-import css from './CurrencyPage.module.scss';
-
 export const CurrencyPage = () => {
   return (
     <>
       <Header />
-       <div className={css.currency}>
-          <div className={css.currency_section}>
-            <div>
-              <Navigation />
-            </div>
-            <div className={css.currency_table}>
-              <Currency />
-            </div>
-          </div>
-        </div>
+      <Navigation />
+      <Currency />
       <Footer />
     </>
   );
